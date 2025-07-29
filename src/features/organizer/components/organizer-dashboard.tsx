c191ffe577b155a2643df863a7fe865815af5fed
mferdinandr <mferdinr@gmail.com>
2025-07-29 14:26:24 +0700

    feat: profile component

--- src/features/organizer/components/organizer-dashboard.tsx
+++ src/features/organizer/components/organizer-dashboard.tsx
@@ -1,27 +1,15 @@
-'use client'
+"use client";
 
-import { Tabs, TabsContent } from '@/components/ui/tabs'
-import ProfileCard from './profile-card'
-import StatCards from './stat-cards'
-import CampaignsTab from './campaigns-tab'
-import RecentDonationsTab from './recent-donations-tab'
-import { useState } from 'react'
-import { Card, CardContent } from '@/components/ui/card'
-import { Button } from '@/components/ui/button'
-
-export const MOCK_USER_DATA = {
-  address: '0x1234567890abcdef1234567890abcdef12345678',
-  publicName: 'Yayasan Peduli Anak',
-  isNameSet: true,
-  isVerified: true,
-  isWhitelisted: false, // For SBT claim
-  hasSBT: true,
-  joinedDate: '2024-01-10',
-  avatar: '/placeholder.svg?height=80&width=80',
-}
+import { Tabs, TabsContent } from "@/components/ui/tabs";
+import StatCards from "./stat-cards";
+import CampaignsTab from "./campaigns-tab";
+import { useState } from "react";
+import { Card, CardContent } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import ProfileCard from "./Profile/profile-card";
 
 export default function OrganizerDashboard() {
-  const [activeTab, setActiveTab] = useState('campaigns')
+  const [activeTab, setActiveTab] = useState("campaigns");
 
   return (
     <div className="container mx-auto px-4 py-8">
@@ -36,16 +24,17 @@ export default function OrganizerDashboard() {
           <Card className="mt-8">
             <CardContent className="space-y-2">
               <Button
-                variant={activeTab === 'campaigns' ? 'ghost-active' : 'ghost'}
+                variant={activeTab === "campaigns" ? "ghost-active" : "ghost"}
                 className="w-full justify-start"
-                onClick={() => setActiveTab('campaigns')}
+                onClick={() => setActiveTab("campaigns")}
               >
                 Kampanye Saya
               </Button>
               <Button
-                variant={activeTab === 'donations' ? 'ghost-active' : 'ghost'}
+                variant={"ghost"}
                 className="w-full justify-start"
-                onClick={() => setActiveTab('donations')}
+                onClick={() => setActiveTab("donations")}
+                disabled
               >
                 Donasi Terbaru
               </Button>
@@ -64,12 +53,12 @@ export default function OrganizerDashboard() {
             <TabsContent value="campaigns" className="space-y-6">
               <CampaignsTab />
             </TabsContent>
-            <TabsContent value="donations" className="space-y-6">
+            {/* <TabsContent value="donations" className="space-y-6">
               <RecentDonationsTab />
-            </TabsContent>
+            </TabsContent> */}
           </Tabs>
         </div>
       </div>
     </div>
-  )
+  );
 }
